e55e6b39eb0afca26676f31c1dfbd6db664625cd
root <144000giovanni@gmail.com>
2021-04-28 01:53:23 +0000

    added jesus-paid-it-all-karaoke.mp4 jesus-paid-it-all-karaoke.mp3

--- dl.sh
+++ dl.sh
@@ -12,4 +12,9 @@ youtube-dl -f mp4 "https://www.youtube.com/watch?v=$videoId" --output "$filename
 
 ffmpeg -i "$filename.mp4" -vn -acodec libmp3lame -ac 2 -ab 160k "$filename.mp3"
 
-bash deploy.sh
+git pull
+git add .
+
+git commit -m "added $filename.mp4 $filename.mp3"
+git push --all
+
