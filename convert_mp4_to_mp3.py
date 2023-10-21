d0580fbe7a5ef160a3fcaa079c6b764186985ab0
gesab001 <144000giovanni@gmail.com>
2023-10-22 07:05:14 +1300

    'update'

--- convert_mp4_to_mp3.py
+++ convert_mp4_to_mp3.py
@@ -7,6 +7,7 @@ def showList():
    if file.endswith(".mp4"):
       #print(os.path.join("/mydir", file))
       jukebox.append(file)
+ jukebox.append("SEARCH") 
  lastNumber = len(jukebox) 
  print("lastNumber: " + str(lastNumber))
  for x in range(0, len(jukebox)):
@@ -24,10 +25,41 @@ def convert(number, jukebox):
   #url = "https://www.youtube.com/watch?v="+videoId
   #subprocess.call("youtube-dl -x --audio-format mp3 "+ url +" --output " + filename_webm, shell=True)
   subprocess.call("ffmpeg -i " + filename_mp4 + " -f mp3 -ab 160000 -vn " + filename_mp3, shell=True)
-  
+
+def searchSong(keyword, jukebox):
+ searchresults = []
+ for song in jukebox:
+    if keyword.lower() in song.lower():
+      searchresults.append(song)
+ searchresults.append("MENU") 
+
+ lastNumber = len(searchresults) 
+ print("lastNumber: " + str(lastNumber))
+ for x in range(0, len(searchresults)):
+   print(str(x) +"."+ "\t" + searchresults[x])  
+ return searchresults  
+ 
 while True:		
    jukebox = showList()
    selection = int(input("select number: "))
-   convert(selection, jukebox)
+   if jukebox[selection]=="SEARCH":
+     keyword = input("search: ")
+     jukebox = searchSong(keyword, jukebox)
+     if len(jukebox)==0:
+       print("no match found")
+       keyword = input("search again? (y/n)")
+       if keyword.lower()=="y":
+         keyword = input("search again? (y/n)")     
+         jukebox = searchSong(keyword, jukebox)       
+         selection = int(input("select number: "))
+         convert(selection, jukebox)   
+     else:  
+       selection = int(input("select number: "))
+       if jukebox[selection]=="MENU":
+          print("back to menu")
+       else:          
+          convert(selection, jukebox)
+   else:
+     convert(selection, jukebox)
 
 
